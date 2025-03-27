df0f7440ccbc2213992f2f5f3b707de9b54bbde4
Dhiraj Sahu <99193435+DhirajSahu18@users.noreply.github.com>
2025-03-28 00:02:45 +0530

    Update participant.model.js

--- model/participant.model.js
+++ model/participant.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const participantSchema = new mongoose.Schema(
     {
       name: { type: String, required: [true, "Name is required"] },
-      email: { type: String, required: [true, "Email is required"] },
+      email: { type: String, required: [true, "Email is required"], unique: true},
       phoneNumber: { type: String },
       fromTerna: { type: String},
       idCardUrl: { type: String }, // Store the Cloudinary file URL
